fb7293a880c78eac27be1c5e782aa71c2bcc1f99
Pioooooo <bowen.sun@qq.com>
2021-04-16 22:17:01 +0800

    fix bash

--- codegen.bash
+++ codegen.bash
@@ -1,4 +1,4 @@
 set ff=UNIX
 set -e
 cp ./lib/builtin.s ./buitin.s
-cat | java -cp /ulib/java/antlr-4.9-complete.jar:./lib/picocli-4.6.1.jar:./bin app.PioMxCompiler -c -o output.s -O
+cat | java -cp /ulib/java/antlr-4.9-complete.jar:./lib/picocli-4.6.1.jar:./bin app.PioMxCompiler -O -o output.s
