0da1e1a3f7bc52e40dcf92fa16a5ba5ab9acbf72
Pioooooo <bowen.sun@qq.com>
2021-04-17 06:29:43 +0800

    codegen for debug

--- codegen.bash
+++ codegen.bash
@@ -2,4 +2,4 @@ set ff=UNIX
 set -e
 cp ./lib/builtin.s ./buitin.s
 cat | java -cp /ulib/java/antlr-4.9-complete.jar:./lib/picocli-4.6.1.jar:./bin app.PioMxCompiler -O -o output.s
-cat output.s
+cat output.s >&2
